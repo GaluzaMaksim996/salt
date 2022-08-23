708742a4dd3ce70382f1152c171bb0b7dcf88a3c
Maksim <krakket@bk.ru>
2022-08-23 15:16:23 +0600

    added smooth scroll

--- app/js/main.js
+++ app/js/main.js
@@ -24,3 +24,16 @@ const swiper = new Swiper('.swiper', {
     },
   }
 });
+
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+  smoothLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = smoothLink.getAttribute('href');
+
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+};
